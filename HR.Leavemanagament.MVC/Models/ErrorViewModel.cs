bb5889f92c0facaa020eb7dc68ed3f76887df312
Agaba Kabunga <akabunga@ruan.com>
2022-01-14 08:55:39 -0600

    add client template code

---
+++ HR.Leavemanagament.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HR.Leavemanagament.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
